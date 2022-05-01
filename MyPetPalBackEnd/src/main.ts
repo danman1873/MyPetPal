6969cdd27302a66131e36331ef8e530afad2868c
danman1873 <92875938+danman1873@users.noreply.github.com>
2022-05-01 08:12:59 -0500

    Update main.ts

--- MyPetPalBackEnd/src/main.ts
+++ MyPetPalBackEnd/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
+  const port = process.env.PORT || 3000
   await app.listen(3000);
 }
 bootstrap();
